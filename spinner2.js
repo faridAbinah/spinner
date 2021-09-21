640b399ef1937b29ed8e117c276b5aa1458db04a
faridAbinah <faridabinah@hotmail.com>
2021-09-21 14:20:49 -0400

    Refactored spinner 1 to have less code and complete the same animation

---
+++ spinner2.js
@@ -0,0 +1,21 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+///r is going to take our cursor back to the beginning
+let delay = 0;
+//We can refactor our spinner code by simply storing all the characters in an array, looping through the array.
+//Every time we loop we will call our setTimeout function increasing the delay so we can see the animation.
+let characters = ["|","/","-","\\","|","/","-", "\\"];
+
+for(const char of characters) {
+  delay += 200;
+
+  setTimeout(() => {
+    process.stdout.write(`\r${char}`);
+  },delay)
+}
+
+
+
+
+// setTimeout(() => {
+//   process.stdout.write('\n');
+// }, 2200);
